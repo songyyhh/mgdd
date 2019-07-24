bccc6e3b55636f8ac371a4ad83fc7901a2cbbb86
songzg <songyyhh@163.com>
2019-07-24 18:30:33 +0800

    mmgd-entity 项目实体

---
+++ mmgd-entity/src/main/java/com/syhh/mmgd/entity/person/Police.java
@@ -0,0 +1,34 @@
+package com.syhh.mmgd.entity.person;
+
+
+public class Police {
+
+  private Integer id;
+  private String name;
+
+  public Police(){
+
+  }
+
+  public Police(Integer id, String name){
+    this.id = id;
+    this.name = name;
+  }
+
+  public Integer getId() {
+    return id;
+  }
+
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+}
---
+++ mmgd-entity/src/main/java/com/syhh/mmgd/entity/person/User.java
@@ -0,0 +1,13 @@
+package com.syhh.mmgd.entity.person;
+
+public class User {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
